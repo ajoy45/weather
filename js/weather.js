c4ec982fc36dd9fe5c26eb47b64abd330ae11c73
ajoy biswas <ajoypust@gmail.comm>
2022-03-03 23:33:31 +0600

    show change

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEy = 'c3f50ba5df26ac35b562878f9b11ee09';
+// console.log(API_KEy);
+const showWeather=()=>{
+    const inputField=document.getElementById('input-field').value;
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${inputField}&appid=${API_KEy}&units=metric`;
+    // console.log(url)
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayWeather(data))
+
+}
+
+const showDetails=(id,text)=>{
+    const cityName=document.getElementById(id);
+    cityName.innerText=text;
+};
+const showIcon=(ID,value)=>{
+    const icon=document.getElementById(ID);
+    icon.setAttribute("src",`http://openweathermap.org/img/wn/${value}@2x.png`);
+    icon.innerText=value;
+}
